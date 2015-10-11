802758a23b6d24d2483812f9318f4cbc7b0fed77
aliaspider <aliaspider@gmail.com>
2015-10-11 02:58:32 +0100

    get rid of all kernel mode dependancies. works from 3dsx, but only from NH1 :/

--- frontend/3ds/3ds_utils.c
+++ frontend/3ds/3ds_utils.c
@@ -3,6 +3,8 @@
 #include "3ds_utils.h"
 #include "../libpcsxcore/new_dynarec/assem_arm.h"
 #include <stdlib.h>
+#include <string.h>
+//#include "khax.h"
 
 typedef s32 (*ctr_callback_type)(void);
 
@@ -86,6 +88,124 @@ void ctr_flush_invalidate_cache(void)
    ctr_invalidate_ICache();
 }
 
+
+
+/* only works if addr is page aligned, and the first 16bytes are
+ * different from the first 16bytes in all other pages in FCRAM */
+
+u32* get_fake_linear_addr(u32* addr)
+{
+   extern Handle gspEvents[GSPEVENT_MAX];
+
+
+//   u32 top_fcram = 0x8000000;
+   u32 top_fcram   = 0x4000000;
+   u32 buffer_size = 0x10000;
+   u32 stepSize    = 0x100;
+
+   u32 pages_to_scan = buffer_size >> 4;
+   u32 skipSize     = stepSize - 0x10;
+   u32* buffer = linearMemAlign(buffer_size, 0x1000);
+
+//   DEBUG_HOLD();
+   addr = (u32*)((u32)addr & ~0xFFF);
+
+   svcSleepThread(1000000);
+
+   u32 start_address;
+   u32* ret = (u32*)-1;
+
+   while (top_fcram >= pages_to_scan * stepSize)
+   {
+      start_address = (u32*)(0x14000000 + top_fcram - pages_to_scan * stepSize);
+
+      svcClearEvent(gspEvents[GSPEVENT_PPF]);
+         GX_SetTextureCopy(NULL, start_address, (skipSize << 16) | 0x10, buffer, 0x10, buffer_size, 0xC);
+      svcWaitSynchronization(gspEvents[GSPEVENT_PPF], U64_MAX);
+
+//      DEBUG_HOLD();
+      int i;
+      u32* ptr = buffer;
+      for (i=0; i < pages_to_scan ; i += (0x1000 / stepSize), ptr += (0x1000 / stepSize) * 0x4)
+         if(!memcmp(ptr, addr, 0x10))
+            break;
+
+      if(i < pages_to_scan)
+      {
+         ret = (u32*)((u32)start_address + i * stepSize);
+         break;
+      }
+
+      top_fcram -= pages_to_scan * stepSize;
+   }
+//   DEBUG_HOLD();
+
+   linearFree(buffer);
+   return ret;
+}
+
+Result GX_SetTextureCopy2(u32* gxbuf, u32* inadr, u32 indim, u32* outadr, u32 outdim, u32 size, u32 flags)
+{
+	if(!gxbuf)gxbuf=gxCmdBuf;
+
+	u32 gxCommand[0x8];
+	gxCommand[0]=0x01000004; //CommandID
+	gxCommand[1]=(u32)inadr;
+	gxCommand[2]=(u32)outadr;
+	gxCommand[3]=size;
+	gxCommand[4]=indim;
+	gxCommand[5]=outdim;
+	gxCommand[6]=flags;
+	gxCommand[7]=0x0;
+
+	return GSPGPU_SubmitGxCommand(gxbuf, gxCommand, NULL);
+}
+
+static Result ctrGuSetCommandList_First(bool queued, u32* buf0a, u32 buf0s, u32* buf1a, u32 buf1s, u32* buf2a, u32 buf2s)
+{
+   u32 gxCommand[0x8];
+   gxCommand[0]=0x05 | (queued? 0x01000000 : 0x0); //CommandID
+   gxCommand[1]=(u32)buf0a; //buf0 address
+   gxCommand[2]=(u32)buf0s; //buf0 size
+   gxCommand[3]=(u32)buf1a; //buf1 address
+   gxCommand[4]=(u32)buf1s; //buf1 size
+   gxCommand[5]=(u32)buf2a; //buf2 address
+   gxCommand[6]=(u32)buf2s; //buf2 size
+   gxCommand[7]=0x0;
+
+   return GSPGPU_SubmitGxCommand(gxCmdBuf, gxCommand, NULL);
+}
+
+
+void GSPwn(void *dest, const void *src, size_t size)
+{
+	// Attempt a flush of the source, but ignore the result, since we may have just been asked to
+	// read unmapped memory or something similar.
+//	GSPGPU_FlushDataCache(NULL, (u32)src & ~0xFFF, (size + 0x1FFF) & ~0xFFF);
+
+//   ctrGuSetCommandList_First(false, src, size, 0,0,0,0);
+	// Invalidate the destination's cache, since we're about to overwrite it.  Likewise, ignore
+	// errors, since it may be the destination that is an unmapped address.
+//	GSPGPU_InvalidateDataCache(NULL, dest, size);
+
+   extern Handle gspEvents[GSPEVENT_MAX];
+//   svcClearEvent(gspEvents[GSPEVENT_PPF]);
+//   svcWaitSynchronization(gspEvents[GSPEVENT_PPF], U64_MAX);
+//   DEBUG_HOLD();
+   svcClearEvent(gspEvents[GSPEVENT_PPF]);
+	if (GX_SetTextureCopy(NULL, src, 0, dest, 0, size, 8))
+		exit(1);
+//   DEBUG_HOLD();
+   svcWaitSynchronization(gspEvents[GSPEVENT_PPF], U64_MAX);
+//   DEBUG_HOLD();
+//   svcClearEvent(gspEvents[GSPEVENT_PPF]);
+//   svcWaitSynchronization(gspEvents[GSPEVENT_PPF], size * 100 );
+   svcSleepThread(1000000);
+//   DEBUG_HOLD();
+
+//   gspWaitForPPF();
+
+}
 extern char translation_cache[1 << 22];
 //extern char * translation_cache;
 extern char translation_cache_w[1 << 22];
@@ -97,15 +217,48 @@ uint32_t currentHandle;
 //char translation_cache[1 << TARGET_SIZE_2] __attribute__((aligned(4096)));
 int ctr_svchack_init(void)
 {
-
-   translation_cache_voffset     = 0x17800000;
-   translation_cache_w_voffset   = 0x16500000;
-   return 1;
-
    extern unsigned int __service_ptr;
 
+//   translation_cache_voffset     = 0x17800000;
+//   translation_cache_w_voffset   = 0x16500000;
+//   if(__service_ptr)
+//   {
+////      translation_cache_voffset     = 0x17CBE000;
+////      translation_cache_w_voffset   = 0x16A52000;
+
+//      translation_cache_voffset     = 0x16952000;
+//      translation_cache_w_voffset   = 0x16952000; //a7 -->
+//   }
+//   ctr_enable_all_svc();
+
+   translation_cache_w[0]= 0x3DF853BC;
+   translation_cache_w[1]= 0x03FB3A2D;
+   translation_cache_w[2]= 0xBCD3AD69;
+   translation_cache_w[3]= 0xF346ABC3;
+//   DEBUG_HOLD();
+
+//   svcFlushProcessDataCache(0xFFFF8001, translation_cache_w, 16);
+   ctrGuSetCommandList_First(false, translation_cache_w, 16, 0,0,0,0);
+//   DEBUG_HOLD();
+
+   translation_cache_voffset   = (u32)get_fake_linear_addr(translation_cache)   - (u32)translation_cache;
+   translation_cache_w_voffset = (u32)get_fake_linear_addr(translation_cache_w) - (u32)translation_cache_w;
+
+   printf("translation_cache         : 0x%08X\n", translation_cache);
+   printf("translation_cache_voffset : 0x%08X\n", translation_cache_voffset);
+   printf("translation_cache_w         : 0x%08X\n", translation_cache_w);
+   printf("translation_cache_w_voffset : 0x%08X\n", translation_cache_w_voffset);
+
+   DEBUG_HOLD();
+
+   return 1;
+
+#if 0
+
    if(__service_ptr)
-      return 0;
+//      return 0;
+      if(khaxInit())
+         return 0;
 
    /* CFW */
    ctr_enable_all_svc();
@@ -162,64 +315,7 @@ int ctr_svchack_init(void)
    DEBUG_HOLD();
 
    return 1;
-}
-Result GX_SetTextureCopy2(u32* gxbuf, u32* inadr, u32 indim, u32* outadr, u32 outdim, u32 size, u32 flags)
-{
-	if(!gxbuf)gxbuf=gxCmdBuf;
-
-	u32 gxCommand[0x8];
-	gxCommand[0]=0x01000004; //CommandID
-	gxCommand[1]=(u32)inadr;
-	gxCommand[2]=(u32)outadr;
-	gxCommand[3]=size;
-	gxCommand[4]=indim;
-	gxCommand[5]=outdim;
-	gxCommand[6]=flags;
-	gxCommand[7]=0x0;
-
-	return GSPGPU_SubmitGxCommand(gxbuf, gxCommand, NULL);
-}
-
-static Result ctrGuSetCommandList_First(bool queued, u32* buf0a, u32 buf0s, u32* buf1a, u32 buf1s, u32* buf2a, u32 buf2s)
-{
-   u32 gxCommand[0x8];
-   gxCommand[0]=0x05 | (queued? 0x01000000 : 0x0); //CommandID
-   gxCommand[1]=(u32)buf0a; //buf0 address
-   gxCommand[2]=(u32)buf0s; //buf0 size
-   gxCommand[3]=(u32)buf1a; //buf1 address
-   gxCommand[4]=(u32)buf1s; //buf1 size
-   gxCommand[5]=(u32)buf2a; //buf2 address
-   gxCommand[6]=(u32)buf2s; //buf2 size
-   gxCommand[7]=0x0;
-
-   return GSPGPU_SubmitGxCommand(gxCmdBuf, gxCommand, NULL);
-}
-
-
-void GSPwn(void *dest, const void *src, size_t size)
-{
-	// Attempt a flush of the source, but ignore the result, since we may have just been asked to
-	// read unmapped memory or something similar.
-//	GSPGPU_FlushDataCache(NULL, (u32)src & ~0xFFF, (size + 0x1FFF) & ~0xFFF);
-
-//   ctrGuSetCommandList_First(false, src, size, 0,0,0,0);
-	// Invalidate the destination's cache, since we're about to overwrite it.  Likewise, ignore
-	// errors, since it may be the destination that is an unmapped address.
-//	GSPGPU_InvalidateDataCache(NULL, dest, size);
-
-   extern Handle gspEvents[GSPEVENT_MAX];
-//   svcClearEvent(gspEvents[GSPEVENT_PPF]);
-//   svcWaitSynchronization(gspEvents[GSPEVENT_PPF], U64_MAX);
-   svcClearEvent(gspEvents[GSPEVENT_PPF]);
-	if (GX_SetTextureCopy(NULL, src, 0, dest, 0, size, 8))
-		exit(1);
-   svcWaitSynchronization(gspEvents[GSPEVENT_PPF], U64_MAX);
-//   svcClearEvent(gspEvents[GSPEVENT_PPF]);
-//   svcWaitSynchronization(gspEvents[GSPEVENT_PPF], size * 100 );
-   svcSleepThread(1000000);
-
-//   gspWaitForPPF();
-
+#endif
 }
 
 //u32* translation_cache_clean = translation_cache;
@@ -228,8 +324,10 @@ void ctr_flush_DCache_range(void* start, void* end)
 //   svcFlushProcessDataCache(0xFFFF8001, (u32)start - (u32)translation_cache + (u32)translation_cache_w, (u32)end - (u32)start);
 //   svcInvalidateProcessDataCache(0xFFFF8001, (u32)start, (u32)end - (u32)start);
 //   GSPGPU_FlushDataCache(NULL, (u32)start - (u32)translation_cache + (u32)translation_cache_w, (u32)end - (u32)start);
+//   DEBUG_HOLD();
    ctrGuSetCommandList_First(false, (u32)start - (u32)translation_cache + (u32)translation_cache_w, (u32)end - (u32)start, 0,0,0,0);
 
+//   DEBUG_HOLD();
    start = (void*)((u32)start & ~0xF);
    end   = (void*)(((u32)end   + 0xF) & ~0xF);
    u32 src = (u32)start - (u32)translation_cache + (u32)translation_cache_w + translation_cache_w_voffset;
@@ -259,7 +357,9 @@ void ctr_flush_DCache_range(void* start, void* end)
 //   printf("GSPwn(0x%08X, 0x%08X, %u)\n", dst, src, size);
 //   DEBUG_HOLD();
 //   ctr_flush_invalidate_cache();
+//   DEBUG_HOLD();
    GSPwn((void*)dst, (void*)src, size);
+//   DEBUG_HOLD();
 //   ctr_flush_invalidate_cache();
 
 //   svcFlushProcessDataCache(0xFFFF8001, start, (u32)(end)-(u32)(start));
